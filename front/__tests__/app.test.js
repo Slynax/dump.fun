6dc683e0a4f84611efc16492f9b768fb5563de97
Crackvignoule <kiki.kalagan@gmail.com>
2025-01-24 20:15:17 +0100

    test

--- front/__tests__/app.test.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-
-test('front-end application is running', async () => {
-  const response = await axios.get('http://localhost:80');
-  expect(response.status).toBe(200);
-});
